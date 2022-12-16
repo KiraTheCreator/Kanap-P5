3acb44b898e3733156b0160f27507d8e0420ca7a
Som Thea <theadeveloppeurweb@gmail.com>
2022-12-16 17:03:29 +0100

    first commit

---
+++ front/js/product.js
@@ -0,0 +1,105 @@
+const idString = window.location.search;
+const urlSearch = new URLSearchParams(idString);
+const itemId = urlSearch.get("id");
+let altTxt;
+let colors;
+let description;
+let imageUrl;
+let itemName;
+let price;
+let _id;
+let image;
+let divImage;
+console.log(idString);
+console.log(itemId);
+
+fetch(`http://localhost:3000/api/products/${itemId}`)
+  .then((response) => response.json())
+  .then((res) => manageData(res));
+
+// reccupereer touttes les données du canap
+function manageData(item) {
+  altTxt = item.altTxt;
+  colors = item.colors;
+  description = item.description;
+  imageUrl = item.imageUrl;
+  itemName = item.name;
+  price = item.price;
+  _id = item._id;
+  insertImage(imageUrl, altTxt);
+  insertTitle(itemName);
+  insertPrice(price);
+  insertDescription(description);
+  insertColors(colors);
+}
+
+function insertImage() {
+  image = document.createElement("img");
+  image.src = imageUrl;
+  image.altTxt = altTxt;
+  divImage = document.getElementsByClassName("item__img");
+  divImage[0].appendChild(image);
+}
+
+function insertTitle() {
+  const h1 = document.getElementById("title");
+  console.log(h1);
+  h1.textContent = itemName;
+}
+
+function insertPrice() {
+  const span = document.getElementById("price");
+  span.textContent = price;
+}
+
+function insertDescription() {
+  const p = document.getElementById("description");
+  p.textContent = description;
+}
+
+// creer variable selectColors et l'associé au select
+// reccuperer les couleurs, creer option, associé les couleurs
+// les mettre dans le select en enfant
+function insertColors() {
+  const selectColors = document.getElementById("colors");
+  colors.forEach((color) => {
+    const option = document.createElement("option");
+    option.value = color;
+    option.textContent = color;
+    selectColors.appendChild(option);
+  });
+}
+
+// --------------------
+// Button ajouter au panier selectionner
+const button = document.getElementById("addToCart");
+// ajout d'un evenement au clic, réccupere la couleur et la quantité selectionné
+button.addEventListener("click", (e) => {
+  const colorSelected = document.getElementById("colors").value;
+  const quantitySelected = document.getElementById("quantity").value;
+  // si couleur ou quantité non selectionné, msg alert
+  if (
+    colorSelected == null ||
+    colorSelected == "" ||
+    quantitySelected == null ||
+    quantitySelected == 0
+  ) {
+    alert(
+      "Veuillez séléctionner une couleur et une quantité pour cet article."
+    );
+  } else {
+    // creation de la structure de l'objet pour le local storage
+    const toAddToCart = {
+      name: itemName,
+      id: _id,
+      color: colorSelected,
+      quantity: quantitySelected * 1,
+      price: price,
+      image: imageUrl,
+      alt: altTxt,
+    };
+    // utilise l'objet créer et l'ajoute au local storage avec tout ses parametres tout en le stringify
+    localStorage.setItem(_id, JSON.stringify(toAddToCart));
+    window.location.href = "cart.html";
+  }
+});
---
+++ front/js/script.js
@@ -0,0 +1,96 @@
+// Communication get avec le serveur, passe les données à la fonction addItems
+fetch("http://localhost:3000/api/products")
+  .then((res) => res.json())
+  .then((data) => {
+    return addItems(data);
+  });
+
+// Ligne 2 a 6 reecrire sans utiliser le then
+
+// 1 - Reccupère les données du premier item, pour créer la const itemId
+// 2 - Appelle la fonction makeItem avec itemId pour crée l'item sous forme de lien
+
+// 3 - Appelle la fonction appendChildren pour réccuperer l'item crée et l'ajoute en tant qu'enfant de itemsList (élément html id #items)
+// ID du canapé
+let itemId;
+// Url de l'image du canapé
+let imageUrl;
+// Texte alt de l'image
+let altTxt;
+// Nom du canapé
+let name;
+// Description du canapé
+let itemDescription;
+// Create element img
+let itemImage;
+// Create element a
+let item;
+// Create element p
+let p;
+// Create element article
+let article;
+// Create element h3
+let h3;
+// Ensemble des canapés
+let itemList;
+
+// Ajoute tout les items dans le dom dans l'ordre da le array
+function addItems(data) {
+  data.forEach((item) => {
+    itemId = item._id;
+    imageUrl = item.imageUrl;
+    altTxt = item.altTxt;
+    itemName = item.name;
+    itemDescription = item.description;
+    itemImage = makeImage(imageUrl, altTxt);
+    item = makeItem(itemId);
+    p = makeDescription(itemDescription);
+    article = makeArticle();
+    h3 = makeTitle(itemName);
+    article.appendChild(itemImage);
+    article.appendChild(h3);
+    article.appendChild(p);
+    appendChildren(item, article);
+  });
+}
+
+// Remplacer make par insert
+
+// Fabrique l'item à partir de l'id de l'item réccupéré précédemment
+function makeItem() {
+  item = document.createElement("a");
+  item.href = "./product.html?id=" + itemId;
+  return item;
+}
+
+// Ajoute l'item en tant qu'enfant de l'élément html #items
+function appendChildren() {
+  itemsList = document.getElementById("items");
+  itemsList.appendChild(item);
+  item.appendChild(article);
+}
+
+function makeImage() {
+  itemImage = document.createElement("img");
+  itemImage.src = imageUrl;
+  itemImage.alt = altTxt;
+  return itemImage;
+}
+
+function makeArticle() {
+  article = document.createElement("article");
+  return article;
+}
+
+function makeTitle() {
+  h3 = document.createElement("h3");
+  h3.textContent = itemName;
+  h3.classList.add("productName");
+  return h3;
+}
+function makeDescription() {
+  p = document.createElement("p");
+  p.textContent = itemDescription;
+  p.classList.add("productDescription");
+  return p;
+}
